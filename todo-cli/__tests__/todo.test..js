ec11093d5473a0b4a87a06743261120827fc456f
root <root@abc.>
2025-04-28 14:50:10 +0000

    Updated

---
+++ todo-cli/__tests__/todo.test..js
@@ -0,0 +1,19 @@
+const todo=require("../todo");
+
+const {all,markAsComplete,add} = todo();
+
+describe("Todo test suite",()=>{
+    test("Should add a todo",()=>{
+        expect(all.length).toBe(0);
+        add({title:"Test todo",dueDate:"2023-03-20",completed:false});
+        expect(all.length).toBe(1);
+    })
+
+    test("Should mark a todo as complete",()=>{
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+    })
+    
+}
+)
