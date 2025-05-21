ebaa88f7312b45403f62e2ddbcf87a1186c71424
root <root@abc.>
2025-05-21 12:28:11 +0000

    lint added

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -2,9 +2,30 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+    env: {
+      node: true,
+      es2021: true,
+      jest: true,
+      commonjs: true,
+    },
+    rules: {},
+  },
 ]);
