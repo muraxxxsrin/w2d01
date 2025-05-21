7a1a2b219fe1c67717eddc0cd7da718cb003f4b6
root <root@abc.>
2025-05-21 12:20:37 +0000

    Husky

--- cli-demo/script.js
+++ cli-demo/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 
--- todo-cli/__tests__/todo.test.js
+++ todo-cli/__tests__/todo.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const todo=require("../todo");
 
 const {all,markAsComplete,add} = todo();
