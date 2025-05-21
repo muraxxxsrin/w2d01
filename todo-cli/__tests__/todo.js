02b4415c97e8dfb930a3ed028e765986e80050f9
root <root@abc.>
2025-05-21 14:53:54 +0000

    Code updated

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const todo=require("../todo");
 
-const {all,markAsComplete,add} = todo();
+const {all,markAsComplete,add,overdue,dueToday,dueLater} = todo();
 
 describe("Todo test suite",()=>{
     test("Should add a todo",()=>{
@@ -15,6 +15,26 @@ describe("Todo test suite",()=>{
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
     })
+
+    test("Should retrieve overdue items", () => {
+    add({ title: "Overdue task", dueDate: "2025-05-19", completed: false });
+    const overdueItems = overdue();
+    expect(overdueItems.length).toBeGreaterThan(0);
+  });
+
+  test("Should retrieve due today items", () => {
+    const today = new Date().toISOString().split("T")[0];
+    add({ title: "Today task", dueDate: today, completed: false });
+    const dueTodayItems = dueToday();
+    expect(dueTodayItems.some(todo => todo.dueDate === today)).toBe(true);
+  });
+
+  test("Should retrieve due later items", () => {
+    add({ title: "Future task", dueDate: "2099-12-31", completed: false });
+    const dueLaterItems = dueLater();
+    expect(dueLaterItems.length).toBeGreaterThan(0);
+  });
+    
     
 }
 )
