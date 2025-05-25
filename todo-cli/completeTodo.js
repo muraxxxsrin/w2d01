4b9f5c9f83b6b05e33bce1c8d7340c31c8c31a1f
root <root@abc.>
2025-05-25 08:27:36 +0000

    Testing husky

--- todo-cli/completeTodo.js
+++ todo-cli/completeTodo.js
@@ -3,7 +3,7 @@
 var argv = require("minimist")(process.argv.slice(2));
 const db = require("./models/index");
 const markAsComplete = async (id) => {
-  try {
+  try { 
     await db.Todo.markAsComplete(id);
   } catch (error) {
     console.error(error);
