e549f9796e13aafa542cee401742c0f77ea1b8c2
root <root@abc.>
2025-05-26 16:20:57 +0000

    todo-app

---
+++ todo-app/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { Todo } = require("./models");
+const bodyParser = require('body-parser');
+
+const app=express();
+
+app.use(bodyParser.json())
+
+app.post("/todos",async(request,response)=>{
+    console.log("Creating a todo:",request.body);
+    try{
+       const todo =  await Todo.addTodo({title:request.body.title,dueDate:request.body.dueDate});
+    return response.json(todo);
+    }catch(error){
+        console.log(error);
+        return response.status(422).json({error:error.message})
+    } 
+    
+})
+
+app.post("/todos/:id/markAsCompleted", async(request,response)=>{
+    console.log("Marking a todo as completed wi id ",request.params.id);
+    
+    try{
+        const todo= await Todo.findByPk(request.params.id);
+        const updatedTodo=await todo.markAsCompleted();
+        return response.json(todo);
+    }
+    catch(error){
+        console.log(error);
+        return response.status(422).json({error:error.message})
+    }
+})
+app.listen(3000,()=>{
+    console.log("server started at port 3000")
+})
